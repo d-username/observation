6bea6865ce65b270649be28f286e8c4988f6af0b
David <czdavid93@gmail.com>
2022-07-18 13:27:48 +0200

    refactored following the feedback

--- observation.js
+++ observation.js
@@ -1,30 +1,29 @@
 // REQ: given an array, and a low and a high threshold. return a new array, rounded the number to be within the threshold.
 
 function keepInThreshold(array, low, high) {
-  let lowestInArray = Math.min(...array);
-  let highestInArray = Math.max(...array);
-  let scoresGiven = [];
-  //map
-  if (low < lowestInArray || high > highestInArray) {
+  if (low >= high) {
     return 'invalid thresholds';
   }
 
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < low) {
-      scoresGiven.push(low);
-    }
-    if (array[i] >= low && array[i] <= high) {
-      scoresGiven.push(array[i]);
-    }
-    if (array[i] > high) {
-      scoresGiven.push(high);
+  let scoresGiven = array.map(checkEachNum);
+
+  function checkEachNum(num) {
+    if (num < low) {
+      return low;
+    } else if (num >= low && num <= high) {
+      return num;
+    } else if (num > high) {
+      return high;
     }
   }
+
   return scoresGiven;
 }
 
-console.log(keepInThreshold([3, 3, 4, 7, 9], 2, 6));
+console.log(keepInThreshold([1, 3, 5, 7, 9], 2, 8));
 
+// [1,3,5,7,9], 2, 8 => [2,3,5,7,8]
 // [3,3,4,7,9], 2, 6 => [3,3,4,6,6]
 // [3,3,4,7,9], 8, 2 => "invalid thresholds"
-// [5,1,9,6,1], 4, 7 = [4,4,5,6,7]
+// [5,1,9,6,1], 4, 7 => [4,4,5,6,7]
+// [3,3,4,5,5], 3, 5 => [3,3,4,5,5]
